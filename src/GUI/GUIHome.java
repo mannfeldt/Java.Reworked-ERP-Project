eb60fffe308328979560113166563b35afccd209
Askeng <askar.engstrom@gmail.com>
2014-12-17 14:47:49 +0100

    Change: Login at start, separate xml for mapper

--- src/GUI/GUIHome.java
+++ src/GUI/GUIHome.java
@@ -240,7 +240,7 @@ public class GUIHome  {
      * this method should be invoked from the
      * event dispatch thread.
      */
-    private static void createAndShowGUI() {
+     public static void createAndShowGUI() {
         //Create and set up the window.
         frame = new JFrame("Hyber");
         frame.setBounds(300, 100, 1200, 537);
@@ -255,29 +255,5 @@ public class GUIHome  {
         frame.setVisible(true);
     }
      
-    public static void main(String[] args) {
-        /* Use an appropriate Look and Feel */
-        try {
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        /* Turn off metal's use of bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
-         
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+    
 }
