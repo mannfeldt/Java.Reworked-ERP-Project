7fcabb36cd8cfcc3b26302ff94cdfa035a54e814
Åskar Engström <askar.engstrom@gmail.com>
2014-12-18 16:23:18 +0100

    Add: Customers to invoice combobox

--- src/controllers/EcoCreateInvoiceCardController.java
+++ src/controllers/EcoCreateInvoiceCardController.java
@@ -4,11 +4,10 @@ import GUI.EcoCreateInvoiceCard;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
-
+import javax.swing.DefaultComboBoxModel;
 import mapper.MyBatisConnectionFactory;
 import mapper.db_Mapper;
 import objects.Customer;
-import objects.Project;
 
 public final class EcoCreateInvoiceCardController implements ActionListener {
 
@@ -25,7 +24,29 @@ public final class EcoCreateInvoiceCardController implements ActionListener {
         String source = event.getActionCommand();
         if (source.equalsIgnoreCase("Save")) {
             System.out.println("Save was pressed");
+            Object selectedCustomer = view.getSelectedCustomer();
+            System.out.println(selectedCustomer);
         }
+    }
+
+    /**
+     * Get all customers
+     * @return a new model containing only customer names
+     */
+    public DefaultComboBoxModel getCustomers() {
+        db_Mapper db_mapper = new db_Mapper
+        (MyBatisConnectionFactory.getSqlSessionFactory());
+        
+        List<Customer> customers;
+        customers = db_mapper.getAllCustomers();
+
+        DefaultComboBoxModel companyNames = new DefaultComboBoxModel();
+
+        for (Customer customer : customers) {
+            companyNames.addElement(customer.getCompanyName());
+
+        }
+        return companyNames;
     }
 
 }
