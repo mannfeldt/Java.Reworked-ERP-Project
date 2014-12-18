2428d3e14e617ff04291faf676bbbe12431cbe56
Åskar Engström <askar.engstrom@gmail.com>
2014-12-18 14:10:07 +0100

    Add: GUI draft for create invoice

--- src/GUI/EcoCreateInvoiceCard.java
+++ src/GUI/EcoCreateInvoiceCard.java
@@ -3,14 +3,90 @@ package GUI;
 import java.awt.Color;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import java.awt.Rectangle;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.SystemColor;
+import javax.swing.JList;
 
 public class EcoCreateInvoiceCard extends JPanel {
+	private JTextField textFieldInvoiceDate;
+	private JTextField textFieldTotalAmount;
 
 	/**
 	 * Create the panel.
 	 */
 	public EcoCreateInvoiceCard() {
 		setBackground(Color.LIGHT_GRAY);
+		setLayout(null);
+		
+		JComboBox comboBoxCustomer = new JComboBox();
+		comboBoxCustomer.setBounds(20, 40, 200, 25);
+		add(comboBoxCustomer);
+		
+		JLabel labelCustomer = new JLabel("Customer");
+		labelCustomer.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		labelCustomer.setBounds(20, 15, 200, 25);
+		add(labelCustomer);
+		
+		JTextArea textAreaBillingAdress = new JTextArea();
+		textAreaBillingAdress.setEditable(false);
+		textAreaBillingAdress.setBounds(20, 110, 200, 100);
+		add(textAreaBillingAdress);
+		
+		JLabel lblBillingAdress = new JLabel("Billing Adress");
+		lblBillingAdress.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblBillingAdress.setBounds(20, 82, 200, 25);
+		add(lblBillingAdress);
+		
+		JComboBox comboBoxProject = new JComboBox();
+		comboBoxProject.setBounds(20, 250, 200, 25);
+		add(comboBoxProject);
+		
+		JLabel lblProject = new JLabel("Project");
+		lblProject.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblProject.setBounds(20, 225, 200, 25);
+		add(lblProject);
+		
+		textFieldInvoiceDate = new JTextField();
+		textFieldInvoiceDate.setBounds(20, 320, 200, 25);
+		add(textFieldInvoiceDate);
+		textFieldInvoiceDate.setColumns(10);
+		
+		JLabel lblInvoiceDate = new JLabel("Invoice Date");
+		lblInvoiceDate.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblInvoiceDate.setBounds(20, 295, 200, 25);
+		add(lblInvoiceDate);
+		
+		JButton btnNewButton = new JButton("Save");
+		btnNewButton.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		btnNewButton.setBounds(130, 400, 90, 35);
+		add(btnNewButton);
+		
+		textFieldTotalAmount = new JTextField();
+		textFieldTotalAmount.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		textFieldTotalAmount.setBounds(300, 320, 200, 25);
+		add(textFieldTotalAmount);
+		textFieldTotalAmount.setColumns(10);
+		
+		JLabel lblTotalAmount = new JLabel("Total Amount");
+		lblTotalAmount.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblTotalAmount.setBounds(300, 295, 200, 25);
+		add(lblTotalAmount);
+		
+		JList listInvoiceLines = new JList();
+		listInvoiceLines.setBounds(300, 40, 200, 235);
+		add(listInvoiceLines);
+		
+		JLabel lblLines = new JLabel("Select lines to add");
+		lblLines.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblLines.setBounds(300, 15, 200, 25);
+		add(lblLines);
+		
 	}
-
 }
